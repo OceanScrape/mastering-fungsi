7dc527456007078f765315c47fc0269cefac768f
OceanScrape <alegoridev@gmail.com>
2021-12-21 08:44:09 +0700

    fungsi

---
+++ membuat_fungsi_segitiga.py
@@ -0,0 +1,16 @@
+def luas_segitiga(a, t):
+    luas = a * t / 2 # ==> proses
+    return luas # ==> proses
+# input
+# proses
+# output
+# untuk mengeluarkan output perlu menggunakan keyword return
+segitiga = luas_segitiga(6, 9) # ==> input
+print(segitiga)
+
+
+
+
+
+
+
